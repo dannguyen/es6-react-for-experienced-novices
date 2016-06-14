5530adaa1f232b6d2c30aee5fb6b34169ca1c80d
Dan Nguyen <dansonguyen@gmail.com>
2016-06-14 13:03:37 -0700

    added server file

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
+// todo fix later
+const defaultSettings = {port: 8080};
+
+const open = require('open');
+const port_number = 8080;
+let target_entry = 'http://localhost:' + defaultSettings.port + '/';
+
+
+new WebpackDevServer(webpack(config), {contentBase: 'src', hot: true, stats: { colors: true }, publicPath: '/assets/'})
+.listen(defaultSettings.port , 'localhost' , (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Listening at localhost:' + defaultSettings.port );
+  console.log('Opening your system browser...');
+  open(target_entry);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
-var path = require("path");
+'use strict';
 
-var srcPath = path.join(__dirname, 'src');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+const srcPath = path.join(__dirname, 'src');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const defaultSettings = {port: 8080};
 
-var config = {
+const config = {
     // context: path.join(__dirname, './src'),
     entry: [
         "babel-polyfill",
         "./src/app/index.js",
-        'webpack-dev-server/client?http://127.0.0.1:8080/',
+        'webpack-dev-server/client?' + 'http://127.0.0.1:' + defaultSettings.port + '/',
         "./src/stylesheets/styles.scss"
     ],
     output: {
         // entry: './src/app/index.js',
         path: path.resolve(__dirname, "build/assets"),
         filename: "bundle.js",
-        publicPath: 'assets/'
+        publicPath: '/assets/'
     }, //...
     module: {
         loaders: [{
